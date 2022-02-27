c994abfb1aaa6f56557ed2795b17745a3d7772f7
Owen <karpfy16@gmail.com>
2022-02-27 15:21:58 -0600

    Added additional metrics about TroFi data

--- TroFi_cleaning.py
+++ TroFi_cleaning.py
@@ -32,20 +32,32 @@ for line in trofi_file:
 vocab = set()
 
 num_metaphor = 0
-total = 0
+total_sentences = 0
+num_tokens = 0
 sentence_tokens_target_label = []
+
 for sentence, target, label in lines:
     if label == 1:
         num_metaphor += 1
     sentence = ' '.join(sentence)
     sentence_tokens = tokenizer.tokenize(sentence)
     for token in sentence_tokens:
+        num_tokens += 1
         vocab.add(token)
     sentence_tokens_target_label.append((sentence_tokens, target, label))
-    total += 1
+    total_sentences += 1
+
+vocab_size = len(vocab)
+percent_metaphors = num_metaphor / total_sentences
+type_to_token = vocab_size / num_tokens
+average_sentence_length = num_tokens / total_sentences
 
 
-print(len(vocab))
-print(num_metaphor/total)
+print("The length of the vocab is: " + str(vocab_size))
+print("The number of sentences is: " + str(total_sentences))
+print("The percentage of sentences that are metaphors is: " + str(percent_metaphors))
+print("The number of tokens is: " + str(num_tokens))
+print("The type to token ratio is: " + str(type_to_token))
+print("The average sentence length is: " + str(average_sentence_length))
 
 
